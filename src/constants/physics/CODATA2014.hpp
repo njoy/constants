ee95b0011c3cd4af95c9bd646167a35eaa410126
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-09-17 17:02:36 -0600

    Working on derived quantities.

--- src/constants/physics/CODATA2014.hpp
+++ src/constants/physics/CODATA2014.hpp
@@ -7,35 +7,57 @@ namespace njoy{
 namespace constants{
 namespace physics{
 
-auto CODATA2014 = hana::make_map(
-  hana::make_pair( avogadro     , 6.022140857e23/mole )           , 
-  hana::make_pair( boltzmann    , 1.38064852e-23*joule/mole )     , 
-  hana::make_pair( elementaryCharge, 1.6021766208e-19*coulomb ),
-  hana::make_pair( G, 
-                   6.67408e-11*meter*meter*meter/kilo( gram )/second/second ),
-  hana::make_pair( newtonianGravitation, 
-                   6.67408e-11*meter*meter*meter/kilo( gram )/second/second ),
-  hana::make_pair( k            , 1.38064852e-23*joule/mole )     , 
-  hana::make_pair( planck       , 6.626070040e-34*joule*second )  , 
-  hana::make_pair( h            , 6.626070040e-34*joule*second )  , 
-  hana::make_pair( hbar         , 1.0545718000e-34*joule*second ) , 
-  hana::make_pair( reducedPlanck, 1.0545718000e-34*joule*second ) , 
-  hana::make_pair( speedOfLight , 2.99792458e8*meter/second )     , 
-  hana::make_pair( c            , 2.99792458e8*meter/second )     , 
+struct PlanckLength : hana::type< PlanckLength >{
+  template< typename MAP >
+  constexpr decltype( auto ) operator()( MAP map ) const {
+    auto tmp = map[ hbar ]*map[ G ]/( map[ c ]*map[ c ]*map[ c ] );
+    return dimwits::math::Pow::evaluate( tmp.value, Ratio<1,2> )*
+        sqrt( tmp.units() );
+  }
+};
+static constexpr PlanckLength planckLength{};
 
-  // Masses
-  hana::make_pair( electronMass , 9.10938356e-31*kilo( gram ) )   , 
-  hana::make_pair( neutronMass  , 1.674927471e-27*kilo( gram ) )  , 
-  hana::make_pair( protonMass   , 1.672621898e-27*kilo( gram ) )  , 
-  hana::make_pair( deuteronMass , 3.343583719e-27*kilo( gram ) )  , 
-  hana::make_pair( helionMass   , 5.0064127000e-27*kilo( gram ) ) , 
-  hana::make_pair( tritonMass   , 5.007356665e-27*kilo( gram ) )  , 
-  hana::make_pair( alphaMass    , 6.644657230e-27*kilo( gram ) )
+struct Insert {
+  template< typename MAP, typename C >
+  constexpr decltype( auto ) operator()( MAP map, C callable ) const{
+    return hana::insert( map, hana::make_pair( callable, callable( map ) ) );
+  }
+};
+static constexpr Insert insert{};
+
+constexpr auto derivedConstants = hana::make_tuple( planckLength );
+
+constexpr auto CODATA2014 = hana::fold( derivedConstants,
+  hana::make_map(
+    hana::make_pair( avogadro     , 6.022140857e23/mole )           , 
+    hana::make_pair( boltzmann    , 1.38064852e-23*joule/mole )     , 
+    hana::make_pair( elementaryCharge, 1.6021766208e-19*coulomb ),
+    hana::make_pair( G, 
+                    6.67408e-11*meter*meter*meter/kilo( gram )/second/second ),
+    hana::make_pair( newtonianGravitation, 
+                    6.67408e-11*meter*meter*meter/kilo( gram )/second/second ),
+    hana::make_pair( k            , 1.38064852e-23*joule/mole )     , 
+    hana::make_pair( planck       , 6.626070040e-34*joule*second )  , 
+    hana::make_pair( h            , 6.626070040e-34*joule*second )  , 
+    hana::make_pair( hbar         , 1.0545718000e-34*joule*second ) , 
+    hana::make_pair( reducedPlanck, 1.0545718000e-34*joule*second ) , 
+    hana::make_pair( speedOfLight , 2.99792458e8*meter/second )     , 
+    hana::make_pair( c            , 2.99792458e8*meter/second )     , 
+
+    // Masses
+    hana::make_pair( electronMass , 9.10938356e-31*kilo( gram ) )   , 
+    hana::make_pair( neutronMass  , 1.674927471e-27*kilo( gram ) )  , 
+    hana::make_pair( protonMass   , 1.672621898e-27*kilo( gram ) )  , 
+    hana::make_pair( deuteronMass , 3.343583719e-27*kilo( gram ) )  , 
+    hana::make_pair( helionMass   , 5.0064127000e-27*kilo( gram ) ) , 
+    hana::make_pair( tritonMass   , 5.007356665e-27*kilo( gram ) )  , 
+    hana::make_pair( alphaMass    , 6.644657230e-27*kilo( gram ) )
+  ), insert 
 );
 
-// CODATA2014[ a ] = 124*kilo( gram );
+// auto CODATA2014_ = hana::fold( tuple, CODATA2014, insert );
 
-auto CODATA2014Uncertainty = hana::make_map(
+constexpr auto CODATA2014Uncertainty = hana::make_map(
   hana::make_pair( avogadro     , 0.000000074e23/mole )          , 
   hana::make_pair( boltzmann    , 0.00000079e-23*joule/mole )    , 
   hana::make_pair( elementaryCharge, 0.0000000098e-19*coulomb ),
