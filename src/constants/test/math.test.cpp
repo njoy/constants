791bd3a2479b600e56bb230a920ed08e4c6cbcc4
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2018-09-18 13:19:35 -0600

    Moving tests to the appropriate location.

---
+++ src/constants/test/math.test.cpp
@@ -0,0 +1,12 @@
+
+#include "catch.hpp"
+
+#include "constants/math.hpp"
+
+SCENARIO( "Testing mathematical constants" ){
+
+  REQUIRE( 3.141592653589793 == njoy::constants::pi );
+  REQUIRE( 2.718281828459045 == njoy::constants::e );
+  REQUIRE( 1.414213562373095 == njoy::constants::root2 );
+
+}
