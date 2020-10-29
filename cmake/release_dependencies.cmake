29bb66bd61cc267a193fb8c9e5514968b37a6cec
Nathan Gibson <ngibson@lanl.gov>
2020-10-29 10:18:49 -0600

    Added release dependencies.

---
+++ cmake/release_dependencies.cmake
@@ -0,0 +1,49 @@
+cmake_minimum_required( VERSION 3.14 )
+include( FetchContent )
+
+#######################################################################
+# Declare project dependencies
+#######################################################################
+
+FetchContent_Declare( range-v3-adapter
+    GIT_REPOSITORY  https://github.com/njoy/range-v3-adapter
+    GIT_TAG         252679d4737c8f755d87c0e1eed6c37394a2ec59
+    )
+
+FetchContent_Declare( hana-adapter
+    GIT_REPOSITORY  https://github.com/njoy/hana-adapter
+    GIT_TAG         f58e8973c9a614dc4f3720b5581a762c61bdbb40
+    )
+
+FetchContent_Declare( spdlog
+    GIT_REPOSITORY  https://github.com/gabime/spdlog
+    GIT_TAG         a51b4856377a71f81b6d74b9af459305c4c644f8
+    )
+
+FetchContent_Declare( catch-adapter
+    GIT_REPOSITORY  https://github.com/njoy/catch-adapter
+    GIT_TAG         fb84b82ebf7a4789aa43cea560680cf745c6ee4f
+    )
+
+FetchContent_Declare( Log
+    GIT_REPOSITORY  https://github.com/njoy/Log
+    GIT_TAG         52962b7796afe37ef1d8f7edb4bf9ecb1b868d15
+    )
+
+FetchContent_Declare( dimwits
+    GIT_REPOSITORY  https://github.com/njoy/DimensionalAnalysis
+    GIT_TAG         acd794d373c8740a788f5c9d58a6eb8ba4d9861a
+    )
+
+#######################################################################
+# Load dependencies
+#######################################################################
+
+FetchContent_MakeAvailable(
+    range-v3-adapter
+    hana-adapter
+    spdlog
+    catch-adapter
+    Log
+    dimwits
+    )
